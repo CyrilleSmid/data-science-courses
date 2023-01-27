3923c9b5fd1830afb57e6f6a7225bc710e03b2f5
CyrilSmed <CyrilSmed@protonmail.com>
2023-01-27 08:50:52 +0500

    Time series baseline

--- utils.py
+++ utils.py
@@ -4,28 +4,18 @@ import plotly.graph_objects as go
 from plotly.offline import init_notebook_mode
 init_notebook_mode(connected = True)
 
-def graph_val_acc_loss(history):
+def plot_val_acc_loss(history, metrics=['loss', 'acc']):
     history_dict = history.history
-    loss_values = history_dict['loss']
-    val_loss_values = history_dict['val_loss']
-    acc = history.history['acc']
-    val_acc = history.history['val_acc']
-
-    fig = make_subplots(rows=2, cols=1)
-    fig.add_trace(go.Scatter(y=loss_values, 
-                            line=dict(color='royalblue', width=6),
-                            name = 'Loss'),
-                row=1, col=1)
-    fig.add_trace(go.Scatter(y=val_loss_values, 
-                            line=dict(color='crimson', width=3),
-                            name = 'Validation Loss'),
-                row=1, col=1)
-    fig.add_trace(go.Scatter(y=acc, 
-                            line=dict(color='dodgerblue', width=6),
-                            name = 'Accuracy'),
-                row=2, col=1)
-    fig.add_trace(go.Scatter(y=val_acc, 
-                            line=dict(color='orange', width=3),
-                            name = 'Validation Accuracy'),
-                row=2, col=1)
+    fig = make_subplots(rows=len(metrics), cols=1)
+    for i, metric in enumerate(metrics):
+        loss_values = history_dict[metric]
+        val_loss_values = history_dict['val' + metric]
+        fig.add_trace(go.Scatter(y=loss_values, 
+                                line=dict(color='royalblue', width=6),
+                                name = metric),
+                    row=i+1, col=1)
+        fig.add_trace(go.Scatter(y=val_loss_values, 
+                                line=dict(color='crimson', width=3),
+                                name = 'Validation ' + metric),
+                    row=i+1, col=1)
     fig.show()
